936fabba264efcbf8f67372e4992b709b644d97a
Xaithe <tgulliver97@gmail.com>
2020-04-21 15:39:30 +0100

    Lesson 4 commit: more REST

--- handlers/products.go
+++ handlers/products.go
@@ -4,12 +4,16 @@ import (
 	"log"
 	"net/http"
 	"products-api/data"
+	"regexp"
+	"strconv"
 )
 
+// Products is a http.Handler
 type Products struct {
 	l *log.Logger
 }
 
+// NewProducts , given a logger, creates a products handler
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
@@ -19,16 +23,78 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		p.getProducts(rw, r)
 		return
 	}
-	// handle PUT (update)
+
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
+	if r.Method == http.MethodPut {
+		// expect id in URI
+		reg := regexp.MustCompile("/([0-9]+)")
+		group := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(group) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(group[0]) != 2 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := group[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		p.updateProduct(id, rw, r)
+		return
+	}
 
 	// catch all
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
 func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+	// fetch products from datastore
 	listproducts := data.GetProducts()
+
+	// serialise to JSON
 	err := listproducts.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to decode JSON", http.StatusBadRequest)
+	}
+
+	data.AddProduct(prod)
+}
+
+func (p *Products) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to decode JSON", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
 	}
 }
